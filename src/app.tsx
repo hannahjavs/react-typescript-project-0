b3b01071e04b32be6bd7322a1df7eca50ed1aec6
hannahjavs <hannahsiujadavji@gmail.com>
2018-06-08 08:35:00 +0100

    WIP

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,43 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import TubeService from './app/timetable/TubeService'
+import TubeList from './app/timetable/TubeList'
 
 class App extends React.Component {
     render() {
         return (
             <div className='app'>
-                <TubeService tube={{
-                    id: 'bakerloo',
-                    name: 'Bakerloo',
-                    modeName: 'tube',
-                    created: '2018-05-30T12:07:19.113Z',
-                    modified: '2018-05-30T12:07:19.113Z',
-                    disruptions: ['smashed window'],
-                    lineStatuses: ['busy'],    
-                }} />
+                <TubeList tubes={[
+                    {
+                        id: 'bakerloo',
+                        name: 'Bakeroo',
+                        modeName: 'Tube',
+                        created: '2018-05-30T12:07:19.113Z',
+                        modified: '2018-05-30T12:07:19.113Z',    
+                        disruptions: ['All clear captain'], 
+                        lineStatuses: ['Spiffing'],
+                    },
+                    // CENTRAL LINE
+                    {
+                        id: 'central',
+                        name: 'Central',
+                        modeName: 'Tube',
+                        created: '2017-05-30T12:07:19.113Z',
+                        modified: '2017-05-30T12:07:19.113Z',    
+                        disruptions: ['Very disrupted'], 
+                        lineStatuses: ['Assiduous'],
+                    },
+                    // NORTHERN LINE
+                    {
+                        id: 'northern',
+                        name: 'Northern',
+                        modeName: 'Tube',
+                        created: '2016-05-30T12:07:19.113Z',
+                        modified: '2016-05-30T12:07:19.113Z',    
+                        disruptions: ['OK'], 
+                        lineStatuses: ['Smelly'],
+                    },
+                ]} />
             </div>
         )
     }
@@ -27,3 +49,7 @@ ReactDOM.render(<App />, document.getElementById('react-app'))
 // make the component render a list of tubes
 // create a store and put dummy data into the store
 // make components read data out of the store
+
+// App - for now just a test component which passes dummy data
+//     TubeList - takes an array of tube data via props and renders...
+//         Tube - takes a single tube via props and renders...
--- src/app/timetable/TubeService.tsx
+++ src/app/timetable/TubeService.tsx
@@ -6,64 +6,6 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Divider from '@material-ui/core/Divider'
 
-// const styles = (theme: Theme) => ({
-//   root: {
-//     width: '100%',
-//     maxWidth: '360px',
-//     backgroundColor: theme.palette.background.paper,
-//   },
-// })
-
-// export interface Props extends WithStyles<'root'> {
-//   tube: {
-//     id : string,
-//     name ?: string,
-//     modeName ?: string,
-//     created ?: string,
-//     modified ?: string,    
-//     disruptions ?: any[], 
-//     lineStatuses ?: any[],
-//   }
-// }
-
-// export class Tube extends React.Component<Props> {
-//   public render() {
-//     const { classes, tube } = this.props
-    
-//     return (
-//       <div className={classes.root}>
-//         <List component='nav'>
-//           <ListItem divider>
-//             <ListItemText primary={tube.id} />
-//           </ListItem>
-//             <ListItemText primary={tube.name} />
-//           <Divider />
-//           <ListItem>
-//             <ListItemText primary={tube.created} />
-//           </ListItem>
-//           <ListItem>
-//             <ListItemText primary={tube.modified} />
-//           </ListItem>
-//           <Divider light />
-//           <ListItem button divider>
-//             <ListItemText primary={tube.modeName} />
-//           </ListItem>
-//           <ListItem divider>
-//             <ListItemText primary={tube.disruptions} />
-//           </ListItem>
-//           <ListItem divider>
-//             <ListItemText primary={tube.lineStatuses} />
-//           </ListItem>
-//         </List>
-//       </div>
-//     )
-//   }
-// }
-
-// export default withStyles(styles)(Tube)
-
-
-// make the component render a list of tubes
 const styles = (theme: Theme) => ({
   root: {
     width: '100%',
@@ -72,7 +14,8 @@ const styles = (theme: Theme) => ({
   },
 })
 
-export interface TubeProps {
+export interface Props extends WithStyles<'root'> {
+  tube: {
     id : string,
     name ?: string,
     modeName ?: string,
@@ -80,55 +23,41 @@ export interface TubeProps {
     modified ?: string,    
     disruptions ?: any[], 
     lineStatuses ?: any[],
+  }
 }
 
-export interface Props extends WithStyles<keyof typeof styles> {
-  tubeTypes : TubeTypes[]
-}
-
-const tubeTypes : TubeProps[] = [
-  // BAKERLOO LINE
-  {
-    id: 'bakerloo',
-    name: 'Bakeroo',
-    modeName: 'Tube',
-    created: '2018-05-30T12:07:19.113Z',
-    modified: '2018-05-30T12:07:19.113Z',    
-    disruptions: 'All clear captain', 
-    lineStatuses: 'Spiffing',
-  },
-  // CENTRAL LINE
-  {
-    id: 'central',
-    name: 'Central',
-    modeName: 'Tube',
-    created: '2017-05-30T12:07:19.113Z',
-    modified: '2017-05-30T12:07:19.113Z',    
-    disruptions: 'Very disrupted', 
-    lineStatuses: 'Assiduous',
-  },
-  // NORTHERN LINE
-  {
-    id: 'northern',
-    name: 'Northern',
-    modeName: 'Tube',
-    created: '2016-05-30T12:07:19.113Z',
-    modified: '2016-05-30T12:07:19.113Z',    
-    disruptions: 'OK', 
-    lineStatuses: 'Smelly',
-  },
-]
-
-export default class Tube extends React.Component<Props, {}> {
+export class TubeService extends React.Component<Props> {
   public render() {
-    const renderedTubeLists = this.props.tubeTypes
-
+    const { classes, tube } = this.props
+    
     return (
+      <div className={classes.root}>
         <List component='nav'>
           <ListItem divider>
-            <ListItemText tubeTypes={tubeTypes} />
+            <ListItemText primary={tube.id} />
+          </ListItem>
+            <ListItemText primary={tube.name} />
+          <Divider />
+          <ListItem>
+            <ListItemText primary={tube.created} />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary={tube.modified} />
+          </ListItem>
+          <Divider light />
+          <ListItem button divider>
+            <ListItemText primary={tube.modeName} />
+          </ListItem>
+          <ListItem divider>
+            <ListItemText primary={tube.disruptions} />
+          </ListItem>
+          <ListItem divider>
+            <ListItemText primary={tube.lineStatuses} />
           </ListItem>
         </List>
+      </div>
     )
   }
 }
+
+export default withStyles(styles)(TubeService)
